893111430fb838829c3020da0b232d476fdeba19
Kyle-2may <kyletwomey99@gmail.com>
2024-09-27 10:15:35 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "Braeunling Consulting",
+  description: "Braeunling Consulting",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+      </head>
+      <body>
+        <Navbar/>
+        <main className ={"relative overflow-hidden"}>
+          {children}
+          </main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="nav-background"> 
+        <div className="nav-text p-4">
+        Braeunling Consulting
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
